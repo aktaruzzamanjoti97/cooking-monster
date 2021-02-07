39209b7982721420768261ed043f46c1f0b0f35e
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-02-07 17:33:34 +0600

    done

--- app.js
+++ app.js
@@ -11,20 +11,22 @@ const searchBtn = document.getElementById('search-btn');
 searchBtn.addEventListener('click', function () {
     const inputMeal = document.getElementById('search-input').value;
     searchByAlphabet(inputMeal);
+
+    document.getElementById('search-input').value = "";
 });
 
 
 //
 const getInputData = meals => {
     const mealsDiv = document.getElementById('meal');
-
+    mealsDiv.innerHTML = "";
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
         mealDiv.className = 'meal-item';
 
         const mealInfo = `
         <div class = "meal-img">
-            <img src = "${meal.strMealThumb}" alt = "food">
+            <img src = "${meal.strMealThumb}" alt = "">
         </div>
         <div class = "meal-name">
             <h3>${meal.strMeal}</h3>
@@ -33,10 +35,13 @@ const getInputData = meals => {
         `;
         mealDiv.innerHTML = mealInfo;
 
-        mealDiv.onclick = function(){
-            displayMealDetail('${meal.strMeal}');
-        }
         mealsDiv.appendChild(mealDiv);
+
+        mealDiv.onclick = function(){
+            displayMealDetail(meal.strMeal);
+        }
+
+
 
     });
         
@@ -48,7 +53,7 @@ const displayMealDetail = name => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
     fetch(url)
     .then(response => response.json())
-    .then(data => renderMealInfo(data.meals[0]))
+    .then(data => renderMealInfo(data.meals[0]));
 
 };
 
@@ -60,6 +65,9 @@ const renderMealInfo = meal => {
     mealDiv.innerHTML = `
     <h2 class="recipe-title">${meal.strMeal}</h2>
     <p class="recipe-category">${meal.strCategory}</p>
+    <div class="recipe-meal-img">
+      <img src="${meal.strMealThumb}" alt="" srcset="">
+    </div>
     <div class="recipe-instruction">
       <h3>Ingredients:</h3>
       <p>${meal.strIngredient1}</p>
@@ -69,9 +77,7 @@ const renderMealInfo = meal => {
       <p>${meal.strIngredient5}</p>
       <p>${meal.strIngredient6}</p>
     </div>
-    <div class="recipe-meal-img">
-      <img src="${meal.strMealThumb}" alt="" srcset="">
-    </div>
+    
     <div class="recipe-order">
       <a href="#" target="_blank">Order Recipe</a>
     </div>
